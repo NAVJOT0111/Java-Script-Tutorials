9a2138969ae64c414712574961332b3cc2c82926
NAVJOT0111 <navjotkaur1440@gmail.com>
2025-02-02 00:56:11 +0530

    added new files

---
+++ counter_app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html", // Adjust this to match the files you want to apply Tailwind to
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
