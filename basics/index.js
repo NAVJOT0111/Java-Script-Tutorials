4e7fa4e18c55b0085cecb6f657ff76c8e9cf647f
NAVJOT0111 <navjotkaur1440@gmail.com>
2025-02-06 18:53:45 +0530

    creating first project in js

---
+++ basics/index.js
@@ -0,0 +1,159 @@
+HEAD
+// // This line is used to print on the console window 
+// console.log('Sat sri akal ji')
+
+// let Name = 'babbar';   // naming a variable
+// console.log(Name);
+
+// const number = 12;     // setting a permanent value to a variable
+
+// console.log(3 >= 3);
+// console.log( 5 === 3);  //checking the equality
+
+// let num = 1;
+// let str = '1';
+// console.log(num === str);
+// console.log(num == str);
+// console.log(true || false);
+// console.log(false || 'babbar');
+// console.log( false || 1);
+
+// // for loop 
+// for(let i = 0; i<5; i = i+1){
+//     console.log("Babbar",i);
+// }
+
+// // while loop 
+// let i = 0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// // do-while loop 
+// let y = 1;
+// do{
+//     console.log(y);
+//     y++;
+// } while(y<10);
+
+
+let rectangle = {
+    length : 2,
+    breadth : 4
+};
+//for-in loop: generally applied on objects
+for (let key in rectangle){
+    // values = rectangle[key]
+    console.log(key,rectangle[key]);
+}
+// for-on loop: generally applied on iterables
+for(let key of Object.keys(rectangle)){       //rectangle is not iterable therefore we use Object.keys(rectangle)
+    console.log(key);
+}
+
+
+// let a = 10;
+// let b = a;
+// a++;
+// console.log(a);
+// console.log(b);
+
+// let a = {value: 10};
+// let b = a;
+// a.value++;
+// console.log(a.value);
+// console.log(b.value);
+
+// let a =10;
+// function inc(a){
+//     a++;
+// }
+// inc(a);
+// console.log(a);
+
+if ('color' in rectangle){
+    console.log('Present');
+}
+else{
+    console.log("Absent");
+// // This line is used to print on the console window 
+// console.log('Sat sri akal ji')
+
+// let Name = 'babbar';   // naming a variable
+// console.log(Name);
+
+// const number = 12;     // setting a permanent value to a variable
+
+// console.log(3 >= 3);
+// console.log( 5 === 3);  //checking the equality
+
+// let num = 1;
+// let str = '1';
+// console.log(num === str);
+// console.log(num == str);
+// console.log(true || false);
+// console.log(false || 'babbar');
+// console.log( false || 1);
+
+// // for loop 
+// for(let i = 0; i<5; i = i+1){
+//     console.log("Babbar",i);
+// }
+
+// // while loop 
+// let i = 0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// // do-while loop 
+// let y = 1;
+// do{
+//     console.log(y);
+//     y++;
+// } while(y<10);
+
+
+let rectangle = {
+    length : 2,
+    breadth : 4
+};
+//for-in loop: generally applied on objects
+for (let key in rectangle){
+    // values = rectangle[key]
+    console.log(key,rectangle[key]);
+}
+// for-on loop: generally applied on iterables
+for(let key of Object.keys(rectangle)){       //rectangle is not iterable therefore we use Object.keys(rectangle)
+    console.log(key);
+}
+
+
+// let a = 10;
+// let b = a;
+// a++;
+// console.log(a);
+// console.log(b);
+
+// let a = {value: 10};
+// let b = a;
+// a.value++;
+// console.log(a.value);
+// console.log(b.value);
+
+// let a =10;
+// function inc(a){
+//     a++;
+// }
+// inc(a);
+// console.log(a);
+
+if ('color' in rectangle){
+    console.log('Present');
+}
+else{
+    console.log("Absent");
+23c79649f520edc8f9dce5fbe3c084143c1a04a4
+}
---
+++ basics/object.js
@@ -0,0 +1,43 @@
+ HEAD
+console.log('Hello ji');
+
+let lastName = 'Babbar';
+let firstName = new String('Love');
+
+let message = 'This is my first message';
+let words = message.split(' ');
+console.log(words);
+
+let msg = `This is 
+        a ' "template
+        literal `;
+console.log(msg);
+
+let date = new Date();
+console.log(date);
+
+let date2 = new Date('June 20 1998 07:18');
+console.log(date2);
+
+
+console.log('Hello ji');
+
+let lastName = 'Babbar';
+let firstName = new String('Love');
+
+let message = 'This is my first message';
+let words = message.split(' ');
+console.log(words);
+
+let msg = `This is 
+        a ' "template
+        literal `;
+console.log(msg);
+
+let date = new Date();
+console.log(date);
+
+let date2 = new Date('June 20 1998 07:18');
+console.log(date2);
+
+23c79649f520edc8f9dce5fbe3c084143c1a04a4
