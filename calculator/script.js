3fe98dc8b85cc0b6b8fb4f19ccd178a9a4fd605d
Navjot Kaur <navjotkaur1440@gmail.com>
2025-02-08 16:24:01 +0530

    created a new project

--- calculator/script.js
+++ calculator/script.js
@@ -6,8 +6,12 @@ Array.from(buttons).forEach((button) => {
             string = eval(string);
             document.querySelector('input').value = string;
         }
-        else{
-            console.log(e.target)
+        else if (e.target.innerHTML  == 'C') {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(e.target);
             string = string + e.target.innerHTML;
             document.querySelector('input').value = string;
         }
