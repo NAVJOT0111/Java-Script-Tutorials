7175bf8ff2ab9e47e29c053597c28a7d5b0519b8
NavX <navjotkaur1440@gmail.com>
2025-02-12 00:58:20 +0530

    Add files via upload

---
+++ BroCode/math.js
@@ -0,0 +1,25 @@
+console.log(Math.PI);
+console.log(Math.E);        //value of exponent
+
+let x = 3.21;
+let y = 2;
+let z;
+
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+
+// z = Math.pow(x,y);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.trunc(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+
+let max = Math.max(x, y, z);
+
+console.log(z);
+console.log(max);
